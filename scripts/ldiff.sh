709ddb2860700478f009c61dba3a273ba9d2c27b
pine318 <pine181818@gmail.com>
2021-02-18 17:41:49 +0900

    スニペット追加，スクリプト修正

--- scripts/ldiff.sh
+++ scripts/ldiff.sh
@@ -2,11 +2,11 @@
 
 dst="tmp"
 outname="ldiff"
-docker_image="pine318/texlive-ja:latest"
 uid="$(id -u):$(id -g)"
+pandocref="ipsj"
 
-# 差分tex作成
-diff_tex=$(docker run -i --rm -u ${uid} -w "$(pwd)" -v "$(pwd):$(pwd)" ${docker_image} latexdiff-vc -e utf8 --git --flatten -t CFONT -r "$@" | cut -d" " -f 4)
+# 差分tex作成(PDF)
+diff_tex=$(docker run -i --rm -u ${uid} -w "$(pwd)" -v "$(pwd):$(pwd)" pine318/texlive-ja:latest latexdiff-vc -e utf8 --git --flatten -t CFONT -r "$@" | cut -d" " -f 4)
 
 if [ $? = 0 ]; then
   # ファイル名から拡張子除去
@@ -18,7 +18,20 @@ if [ $? = 0 ]; then
   # ファイルを移動
   mkdir -p ${dst}/${diffid}
   mv ${outname}.* ${dst}/${diffid}
-  cd ${dst}/${diffid}
-  # 中間ファイル削除
-  ls | grep -v -E ".pdf$" | grep -v -E ".tex$" | xargs rm
+  # pdf以外削除
+  ls -d ${dst}/${diffid}/* | grep -v -E ".pdf$" | xargs rm
+fi
+
+# 差分tex作成(docx)
+diff_tex=$(docker run -i --rm -u ${uid} -w "$(pwd)" -v "$(pwd):$(pwd)" pine318/texlive-ja:latest latexdiff-vc -e utf8 --git --flatten -t BOLD -r "$@" | cut -d" " -f 4)
+
+if [ $? = 0 ]; then
+  # ファイル名から拡張子除去
+  diffid=${diff_tex%.tex}
+  # ファイル名変更
+  mv ${diff_tex} ${outname}.tex
+  # docx化
+  pandoc -d pandoc/${pandocref}/tex_to_docx.yml ${outname}.tex -o ${dst}/${diffid}/${outname}.docx
+  # tex削除
+  rm ${outname}.tex
 fi
